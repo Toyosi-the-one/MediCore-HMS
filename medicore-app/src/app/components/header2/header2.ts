ba9f3cb3a5c373dbac801e7a30d9ccd3c003cbb0
Ijimakin Olutoyosi <oijimakin.2200828@stu.cu.edu.ng>
2026-05-21 08:45:28 +0100

    feature/created admin-dashboard and signup page and home page

---
+++ medicore-app/src/app/components/header2/header2.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header2',
+  imports: [],
+  templateUrl: './header2.html',
+  styleUrl: './header2.scss',
+})
+export class Header2 {}
