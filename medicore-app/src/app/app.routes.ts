87b99bec26265da537a0fc8d206c7af1ad4c6402
Ijimakin Olutoyosi <oijimakin.2200828@stu.cu.edu.ng>
2026-05-21 08:51:42 +0100

    feature/added some extras i forgot to stage

--- medicore-app/src/app/app.routes.ts
+++ medicore-app/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Homepage } from './pages/homepage/homepage';
+import { SignupPage } from './pages/signup-page/signup-page';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AdminDashboard,
+  },
+  {
+    path: 'home',
+    component: Homepage,
+  },
+  {
+    path: 'signup',
+    component: SignupPage,
+  },
+];
---
+++ medicore-app/src/app/pages/admin-dashboard/admin-dashboard.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Header2 } from '../../components/header2/header2';
+
+@Component({
+  selector: 'app-admin-dashboard',
+  imports: [Header2],
+  templateUrl: './admin-dashboard.html',
+  styleUrl: './admin-dashboard.scss',
+})
+export class AdminDashboard {}
