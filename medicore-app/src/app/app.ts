3f8422f65d94c599d895ecf7d7ad39de852615c6
David <davidudechukwu19@gmail.com>
2026-06-02 08:46:50 +0100

    Describe what you changed

--- medicore-app/src/app/app.ts
+++ medicore-app/src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { ToastComponent } from './shared/toast/toast.component';
+import { SeedService } from './services/seed.service';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, ToastComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('medicore-app');
+export class App implements OnInit {
+  constructor(private seedService: SeedService) {}
+
+  ngOnInit(): void {
+    this.seedService.seedIfEmpty().catch(console.error);
+  }
 }
